3e4b743d77ccae1f628c13ad4ad67929630a8717
gongruo.lgr <gongruo.lgr@alibaba-inc.com>
2022-02-04 23:30:08 +0800

    修正nacos

--- bin/start_test.sh
+++ bin/start_test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-auth.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-biz.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-gateway.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-leaf.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-multishop.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-order.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-payment.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-platform.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-rbac.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-search.jar
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-user.jar
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-auth.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-biz.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-gateway.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-leaf.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-multishop.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-order.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-payment.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-platform.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-rbac.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-search.jar &
+java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-user.jar
