44038f07e30955b4a65b52f8bdf34a4d774fd365
gongruo.lgr <gongruo.lgr@alibaba-inc.com>
2022-02-07 01:35:32 +0800

    修正start

--- bin/start_test.sh
+++ bin/start_test.sh
@@ -2,11 +2,11 @@
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-auth.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-biz.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-gateway.jar &
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-leaf.jar
+nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-leaf.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-multishop.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-order.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-payment.jar &
-nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-platform.jar
+nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-platform.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-rbac.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-search.jar &
 nohup java -Djava.security.egd=file:/dev/./urandom -jar mall4cloud-user.jar &
